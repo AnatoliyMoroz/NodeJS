751917a200caaefedd8669169686877b8794c2e4
AnatoliyMoroz <world.wild.cat@gmail.com>
2020-05-22 22:43:15 +0300

    click button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Counter from './Counter'
+import CounterForm from "./CounterForm";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter cnt={4} />
+        <Counter cnt={counter}/>
+        <CounterForm  onChange={()=>setCounter( counter +1 ) }/>
         <p>Template
           Edit <code>src/App.js</code> and save to reload.
         </p>
